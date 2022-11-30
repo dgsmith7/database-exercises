eaf2156d54442bf9ba7c309c3b1afecf6bbb09ab
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-30 15:05:20 -0600

    completed delete exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,10 @@
 USE codeup_test_db;
 SELECT 'Albums released after 1991:' AS 'Info';
 SELECT name FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 SELECT 'Disco albums:' AS 'Info';
 SELECT name FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE genre = 'Disco';
 SELECT 'Pink Floyd albums:' AS 'Info';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
+DELETE FROM albums WHERE artist = 'Pink Floyd';
