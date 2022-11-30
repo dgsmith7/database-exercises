cbdd91aa67697cdfab925d08183845b702fc09a9
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-30 14:54:38 -0600

    completed update exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,7 +1,13 @@
 USE codeup_test_db;
 SELECT 'All albums:' as 'Info';
 SELECT * FROM albums;
+UPDATE albums SET sales = (sales * 10);
+SELECT * FROM albums;
 SELECT 'Albums from before 1980:' AS 'info';
 SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums SET release_date = (release_date - 100) WHERE release_date < 1980;
+SELECT 'Albums from before 1980:' AS 'info';
 SELECT 'Albums by Michael Jackson' AS 'Info';
-SELECT * FROM albums WHERE aritst = 'Michael Jackson';
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
