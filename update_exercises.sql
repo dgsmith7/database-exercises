53a6eab25bb2fb8be3b2a217ac34f88125803376
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-30 14:44:23 -0600

    added step 2 update exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,1 +1,7 @@
 USE codeup_test_db;
+SELECT 'All albums:' as 'Info';
+SELECT * FROM albums;
+SELECT 'Albums from before 1980:' AS 'info';
+SELECT * FROM albums WHERE release_date < 1980;
+SELECT 'Albums by Michael Jackson' AS 'Info';
+SELECT * FROM albums WHERE aritst = 'Michael Jackson';
