18dabdeb17da91cd31baab997a12c03d2c3a27b4
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-12-02 13:47:27 -0600

    completed alias exercises

--- tryStuff.sql
+++ tryStuff.sql
@@ -1,6 +1,33 @@
 USE employees;
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+LIMIT 5;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+ORDER BY full_name
+LIMIT 5;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 5;
+
 SELECT hire_date, COUNT(*)
 FROM employees
 GROUP BY hire_date
 ORDER BY COUNT(*) DESC
 LIMIT 10;
+
+SELECT hire_date, COUNT(*) as number_hired
+FROM employees
+GROUP BY hire_date
+ORDER BY number_hired DESC
+LIMIT 10;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees AS emp
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 5;
