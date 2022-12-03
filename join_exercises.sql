e6bfbd8677fa4fea1e3f216657f4e1a04a17c223
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-12-03 14:41:58 -0600

    gave up on bonus querry - close - partial credit?

--- join_exercises.sql
+++ join_exercises.sql
@@ -25,14 +25,28 @@ USE employees;
 -- WHERE cde.dept_no = 'd009' AND t.to_date = '9999-01-01'
 -- GROUP BY t.title;
 
-SELECT DISTINCT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
-FROM departments AS d
+-- SELECT DISTINCT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+-- FROM departments AS d
+--          JOIN dept_manager AS dm
+--               ON dm.dept_no = d.dept_no
+--          JOIN employees AS e
+--               ON dm.emp_no = e.emp_no
+--          JOIN salaries AS s
+--               ON e.emp_no = s.emp_no
+-- WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+-- GROUP BY d.dept_name, CONCAT(e.first_name, ' ', e.last_name), s.salary
+-- ORDER BY d.dept_name;
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name AS 'Department Name',  CONCAT(e2.first_name, ' ', e2.last_name) AS 'Department Manager'
+FROM employees AS e
+         JOIN current_dept_emp AS cde
+              ON e.emp_no = cde.emp_no
+         JOIN departments AS d
+            ON cde.dept_no = d.dept_no
          JOIN dept_manager AS dm
-              ON dm.dept_no = d.dept_no
-         JOIN employees AS e
-              ON dm.emp_no = e.emp_no
-         JOIN salaries AS s
-              ON e.emp_no = s.emp_no
-WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
-GROUP BY d.dept_name, CONCAT(e.first_name, ' ', e.last_name), s.salary
+              ON d.dept_no = dm.dept_no
+         JOIN employees AS e2
+              ON dm.emp_no = e2.emp_no
+WHERE cde.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+GROUP BY CONCAT(e.first_name, ' ', e.last_name), d.dept_name, CONCAT(e2.first_name, ' ', e2.last_name)
 ORDER BY d.dept_name;
