1e6135290f81278c786fc897ac3f14c3bf45d7e3
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-12-03 13:37:11 -0600

    completed join exercises working on bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -25,4 +25,14 @@ USE employees;
 -- WHERE cde.dept_no = 'd009' AND t.to_date = '9999-01-01'
 -- GROUP BY t.title;
 
-SELECT
+SELECT DISTINCT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+FROM departments AS d
+         JOIN dept_manager AS dm
+              ON dm.dept_no = d.dept_no
+         JOIN employees AS e
+              ON dm.emp_no = e.emp_no
+         JOIN salaries AS s
+              ON e.emp_no = s.emp_no
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+GROUP BY d.dept_name, CONCAT(e.first_name, ' ', e.last_name), s.salary
+ORDER BY d.dept_name;
